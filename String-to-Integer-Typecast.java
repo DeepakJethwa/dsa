256bebff761881cce24faf57a88abe232f7c1ce0
deepak jethwa <deepakjethwa95@gmail.com>
2023-03-09 18:48:50 +0530

    updated

--- String-to-Integer-Typecast.java
+++
@@ -1,5 +0,0 @@
-static int checkConevrtion(String S){
-    //Enter your code here
-    int i=Integer.parseInt(S);
-    return i;
-  }
--- sumandmean.java
+++
@@ -1,8 +0,0 @@
-static void SumAndMean(int arr[],int n){
-    int sum = 0;
-          for (int i = 0; i < n; i++) {
-              sum += arr[i];
-          }
-  
-          System.out.print(sum + " " + (sum / n));
-      }
